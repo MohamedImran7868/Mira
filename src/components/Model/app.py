0162f3e960e0f9235c3e0a53d2d1b34434503aba
MohamedImran7868 <1211101935@student.mmu.edu.my>
2025-06-11 19:49:47 +0800

    Updated the app CORS with origin

--- src/components/Model/app.py
+++ src/components/Model/app.py
@@ -4,7 +4,7 @@ from flask_cors import CORS
 import logging
 
 app = Flask(__name__)
-CORS(app, resources={r"/*": {"origins": ["http://localhost:5173", "https://www.mirahub.me/"]}})
+CORS(app, origins=["http://localhost:5173", "https://www.mirahub.me/"])
 
 # Initialize MIRA only once when the app starts
 mira = MIRA()
